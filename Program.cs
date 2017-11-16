51b324111e7ca549f630acbc65b723a00e4f8a85
Spatchicus <32968404+Spatchicus@users.noreply.github.com>
2017-11-16 19:54:58 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PlayGame game = new PlayGame();
+            bool playAgain = false;
+
+            do
+            {
+                game.PlayGuessingGame();
+                playAgain = game.AskToPlayAgain();
+
+            } while (playAgain);
+        }
+    }
+}
+
+
